fb6e475523b56c6b3fed2682349dd75089643ec8
Nick Bailey <npb596@gmail.com>
2023-02-06 16:03:08 -0600

    Organizing scripts into folders

---
+++ Genome_Analysis/GenWin_SNPS.R
@@ -0,0 +1,15 @@
+library('GenWin')
+setwd('~/Desktop/Reinforcement_Project/VCFS/filtered_vcfs/SNP_vcfs')
+require(data.table)
+
+for (CHROM in 1:18)
+{
+  Filename = paste('Reinforcement.chr', CHROM, '.MAF.txt.gz', sep = "")
+  File = fread(Filename)
+  Spline = paste('Reinforcement.chr', CHROM, '.MAF.Spline.txt', sep = "")
+  SplineList <- assign(Spline, splineAnalyze(Y = File$V3, smoothness = 100,
+                       map = File$V2, plotRaw = FALSE,
+                       plotWindows = FALSE, method = 4))
+  write.table(SplineList$windowData, file=Spline, quote=FALSE,
+              row.names = FALSE, sep = '\t')
+}
